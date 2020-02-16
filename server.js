02c5bf68f5027a6101ba4527d40bbd616c6b8022
Robert Davidson <davidsonrobert1996@gmail.com>
2020-02-15 18:34:17 -0600

    added core structure

---
+++ server.js
@@ -0,0 +1,40 @@
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+
+var express = require("express");
+var app = express();
+
+var routes = require("./contorollers/controller");
+
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"})
+);
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+const MONGODB_URI = "mongodb://localhost:27017/mlbArticles" 
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("Goose got me loose!");
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Rockin' and Rollin' on PORT " + port);
+});
